32171e4487a2d8c82ffed4f2ecd106a81aa6f006
pengqixing <pqixing@163.com>
2017-08-04 14:33:54 +0800

    增加kts文件

--- app/src/main/java/com/pqixing/kotlin/MainActivity.kt
+++ app/src/main/java/com/pqixing/kotlin/MainActivity.kt
@@ -3,12 +3,30 @@ package com.pqixing.kotlin
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.pqixing.utils.Q
+import com.pqixing.utils.log
+import com.pqixing.utils.show
+import com.pqixing.utils.utils.Share
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val str by lazy { "" }
+    val KEY_LOGIN_TIMES = "key_login_times"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Q.init(application)
+
+        var times by Share(KEY_LOGIN_TIMES, 0)
+        "这是第${times++} 次登录".log().show()
+
+        arrayListOf(1, 2)
+                .filter { it == 2 }
+                .map { it to it + 2 }
+                .subList(0, 1)
+                .firstOrNull { it.second == 4 }
+                ?.second ?: return.toString().log()
+        textView.text = "0000"
+        str.log()
     }
+
 }
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript{
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${extra.properties["gradle_version"]}")
+        classpath(kotlinModule("gradle-plugin"))
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra.properties["kotlin_version"]}")
+        print("Properties values = ${extra.properties}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
